5263a8c98cc83b56c2d1168743b14a660a953239
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-11-10 16:04:46 -0400

    RefundItem component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
+import { RefundItem } from "../components/RefundItem"
+
 import SearchSvg from "../assets/search.svg"
+import { CATEGORIES } from "../utils/categories"
+
+const REFUND_EXAMPLE = {
+    id: "123",
+    name: "Marcos",
+    category: "Transporte",
+    amount: "34,50",
+    categoryImg: CATEGORIES['transport'].icon
+}
 
 export function DashBoard() {
     const [name, setName] = useState("")
@@ -24,6 +35,9 @@ export function DashBoard() {
                 </Button>
             </form>
 
+            <div>
+                <RefundItem data={REFUND_EXAMPLE} />
+            </div>
         </div>
     )
 }
