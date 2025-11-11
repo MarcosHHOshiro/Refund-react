8a12b1924bc4ddb4c72a3c714d629ee1a1f99c08
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-11-11 07:52:53 -0400

    creating Pagination component and pagination handle

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react"
+import { use, useState } from "react"
+
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 import { RefundItem } from "../components/RefundItem"
+import { Pagination } from "../components/Pagination"
 
 import SearchSvg from "../assets/search.svg"
 import { CATEGORIES } from "../utils/categories"
@@ -17,11 +19,27 @@ const REFUND_EXAMPLE = {
 
 export function DashBoard() {
     const [name, setName] = useState("")
+    const [page, setPage] = useState(1)
+    const [totalOfPage, setTotalOfPage] = useState(10)
 
     function fetchRefunds(e: React.FormEvent) {
         e.preventDefault()
 
         console.log(name)
+    }
+
+    function handlePagination(action: "next" | "previous") {
+        setPage((prevPage) => {
+            if (action === "next" && prevPage < totalOfPage) {
+                return prevPage + 1;
+            }
+
+            if (action === "previous" && prevPage > 1) {
+                return prevPage - 1;
+            }
+
+            return prevPage
+        })
     }
 
     return (
@@ -39,6 +57,13 @@ export function DashBoard() {
             <div className="mt-6 flex flex-col gap-4 max-h-[342px] overflow-y-scroll">
                 <RefundItem data={REFUND_EXAMPLE} />
             </div>
+
+            <Pagination
+                current={page}
+                total={totalOfPage}
+                onNext={() => handlePagination("next")}
+                onPrevious={() => handlePagination("previous")}
+            />
         </div>
     )
 }
