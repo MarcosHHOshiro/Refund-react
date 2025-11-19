711fd437ffaa8a97301a3e9217acf476baea9b14
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-11-19 16:45:00 -0400

    Local storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -5,13 +5,17 @@ type AuthContext = {
     save: (data: UserAPIResponse) => void
 }
 
+const LOCAL_STORAGE_KEY = "@refund"
+
 export const AuthContext = createContext({} as AuthContext)
 
 export function AuthProvider({ children }: { children: ReactNode }) {
     const [session, setSession] = useState<null | UserAPIResponse>(null)
 
     function save(data: UserAPIResponse) {
-        console.log(data)
+        localStorage.setItem(`${LOCAL_STORAGE_KEY}:user`, JSON.stringify(data.userWithoutPassword))
+        localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token)
+        
         setSession(data)
     }
 
